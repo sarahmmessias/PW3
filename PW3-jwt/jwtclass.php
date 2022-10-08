2732e92c26458083f673dedc4d55fa3178adad7a
Sarah Moreira Messias <79113261+sarahmmessias@users.noreply.github.com>
2022-10-08 20:57:32 -0300

    Update jwtclass.php

--- PW3-jwt/jwtclass.php
+++ PW3-jwt/jwtclass.php
@@ -1,79 +1,66 @@
 <?php
-require_once "conexao.php";
+    class myJWT {
 
-class MyJWT {
-    private $senha = "SenhaSecreta";
-    private $conn = null;
+        private $senha = "SenhaSecreta";
+        public $time;
 
-    function __construct() {
-        $connection = new Conn();
-        $this->conn = $connection->connect();
-    }
+        public function criaToken($payload) {
 
-    public function criaToken($payload){
-        $header = [
-            'alg' => 'SHA256',
-            'typ' => 'JWT'
-         ];
-         
-         $header = json_encode($header);
-         $header = base64_encode($header);
+            $header = [
+                'alg' => 'SHA256',
+                'typ' => 'JWT'
+            ];
+            
+            $header = json_encode($header);
+            $header = base64_encode($header);
 
-         $payload = json_encode($payload);
-         $payload = base64_encode($payload);
-        
-         $signature = hash_hmac('sha256',"$header.$payload",$this->senha,true);
-         $signature = base64_encode($signature);
-        
-         return "$header.$payload.$signature";
-    }
-    public function validaToken($jwt, $expires){
-         $part = explode(".",$jwt);
-         $header = $part[0];
-         $payload = $part[1];
-         $signature = $part[2];
-        
-         $signatureCheck = hash_hmac('sha256',"$header.$payload",$this->senha,true);
-         $signatureCheck = base64_encode($signatureCheck);
-         if ($signature == $signatureCheck){
-            if($expires == true) {
-                $payload = base64_decode($payload);
-                $payload = json_decode($payload, true);
-         
-                 if($payload['exp'] >= time()) {
-                    $retorno = true;
-                 } else {
-                    $retorno = false;
-                 }
-            } else {
+            $this -> time = $payload['time'];
+            
+            $payload = json_encode($payload);
+            $payload = base64_encode($payload);
+            
+            $signature = hash_hmac('sha256', "$header.$payload", $this->senha, true);
+            $signature = base64_encode($signature);
+            
+            return "$header.$payload.$signature";
+
+        }
+
+        public function validaToken($jwt) {
+
+            $part = explode(".", $jwt);
+            $header = $part[0];
+            $payload = $part[1];
+            $signature = $part[2];
+            
+            $signatureCheck = hash_hmac('sha256', "$header.$payload", $this->senha, true);
+            $signatureCheck = base64_encode($signatureCheck);
+            if ($signature == $signatureCheck) {
                 $retorno = true;
+            } else {
+                $retorno = false;
             }
-         }else {
-            $retorno = false;
-         }
-        
-         return $retorno;
+            
+            return $retorno;
+
+        }
+
+        public function refreshToken($jwt) {
+
+            $tempo = $this -> time;
+
+            while (time() < $tempo + 2) {}
+
+            echo '<br> refresh';
+            $this -> criaToken($jwt);
+
+            header('url="receive_form.php"; refresh=0');
+
+            //echo $tempo;
+
+
+        }
+
     }
 
-    public function extractToken($token, $pos) {
-      $token_extracted = explode(".",$token);
-      $payloadData = base64_decode($token_extracted[$pos]);
-      $payloadData = json_decode($payloadData, true);
-      print_r($payloadData);
-
-      return $payloadData;
-
-    }
-
-    public function getBlacklist($token) {
-      $q = "SELECT * FROM token WHERE refresh_token = '".$token."'";
-      $sql = mysqli_query($this->conn, $sql);
-      return $sql->num_rows;
-    }
-    public function insertBlackList($hash, $id) {
-        $q = "INSERT INTO token (refresh_token, id_user) VALUES ('$hash', $id)";
-        $sql = mysqli_query($this->conn, $q);
-        return $sql;
-    }
-}
 ?>
