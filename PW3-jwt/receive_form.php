95b537eb400d4fd03bebec2f79ab0e7cfb8da6a5
Sarah Moreira Messias <79113261+sarahmmessias@users.noreply.github.com>
2022-10-08 20:56:08 -0300

    Update receive_form.php

--- PW3-jwt/receive_form.php
+++ PW3-jwt/receive_form.php
@@ -1,73 +1,69 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html>
+    <body>
 
-<body>
-    
-    <?php 
+        <?php
 
-        require_once "conexao.php";
-        include 'jwtclass.php';
+            $user = "root";
+            $pass = "";
+            $db = "progweb3";
+            $server = "localhost";
+            $conn = mysqli_connect($server, $user, $pass, $db);
 
-        $conn = new Conn();
-        $sql = $conn->connect();
-        $jwt = new MyJWT();
-
-        $user = $_POST['user'];
-        $pass = $_POST['password'];
-
-        if(empty($user) || empty($pass)) {
-            echo "<h1>Campos vazios</h1>";
-        } else {
-
-            $q = "SELECT * FROM usuarios WHERE idusuario='$user' AND senhausuario='$pass' ";
-            $result = $sql->query($q);
-            if ($result->num_rows > 0) {
-                $result = $result->fetch_assoc();
-                print_r($result);
-                $exp = time() + 60;
-                $access_payload = [
-                    "iss"=>"localhost",
-                    "token"=>"access_token",
-                    "id"=>$result['codusuario'],
-                    "nome"=> $result['nomeusuario'],
-                    "email"=>$result['email'],
-                    "exp"=>$exp
-                ];
-
-                $access_token = $jwt->criaToken($access_payload);
-
-                $exp = time() + 120;
-                $refresh_payload = [
-                    "iss"=>"localhost",
-                    "token"=>"refresh_token",
-                    "id"=>$result['codusuario'],
-                    "nome"=> $result['nomeusuario'],
-                    "email"=>$result['email'],
-                    "exp"=>$exp
-                ];
-
-                $refresh_token = $jwt->criaToken($refresh_payload);
-
-                echo "<h2>Tokens:</h2>";
-
-                echo "<p>Access_token: $access_token</p>";
-                echo "<p>Refresh_token: $refresh_token</p><br>";
-
-                echo "<h2>Validade do token</h2><ul><li>se <strong>1</strong> = válido<br></li><li>se <strong>0</strong> = expirado ou inválido</li></ul>";
-
-                echo "<p>Validade access: <strong>".$jwt->validaToken($access_token,true)."</strong></p>";
-                echo "<p>Validade refresh: <strong>".$jwt->validaToken($refresh_token, true)."</strong></p>";
-
+            if($conn->connect_errno)
+            {
+                die("Erro de conexão" . $conn->connect_error);
             } else {
-                echo "usuário ou senha incorretos";
+                echo("Conexão ok");
             }
-        }
-    ?>
-</body>
+
+            require('jwtclass.php');
+
+            $myjwt = new myJWT();
+            
+            $idUsuario = $_POST["usuario"];
+            $senhaUsuario = $_POST["senha"];
+            $sql = "select * from usuarios where idusuario = '". $idUsuario ."' and senhausuario = '". $senhaUsuario ."'";
+            $resultadoQuery = mysqli_query($conn, $sql);
+            if ($resultadoQuery->num_rows == 0){
+                die("usuário ou senha inválidos");
+            }
+            $arrayQuery = $resultadoQuery->fetch_assoc();
+            echo "<BR>";
+            echo "usuário digitado: " . $arrayQuery["idusuario"];
+            echo "<BR>";
+            echo "<BR>";
+            echo "senha digitada: " . $arrayQuery["senhausuario"];
+            
+            $payload = [
+                'iss' => 'localhost',
+                'nome' => $arrayQuery["nomeusuario"],
+                'email' => $arrayQuery["email"],
+                'time' => time()
+            ];
+
+            $time = time();
+            
+            echo "<BR>";
+            echo "<BR>";
+            $token = $myjwt->criaToken($payload);
+            echo $token;
+            
+            echo "<BR>";
+            echo "<BR>";
+            echo "Token validado com sucesso?<br>";
+            if ($myjwt->validaToken($token)){
+                echo "sim";
+
+                while (time() < $time + 2) {}
+
+                $myjwt -> refreshToken($token);
+
+                
+            }else{
+                echo "não";
+            }
+            
+        ?>
+
+    </body>
 </html>
